04b1a906ed9c3a805d347bed62464f2a5a1beddc
thuryaaldj <thuryaamen@gmail.com>
2025-07-01 22:00:57 +0300

    تعديل المشاريع وإضافة db.json

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { FaPhone, FaEnvelope, FaLinkedin,FaGithub,FaFilePdf, FaTimes } from "react-icons/fa";
+
 const Contact = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <section className="contact-section">
       <div className="contact-wrapper">
@@ -8,8 +13,51 @@ const Contact = () => {
           I’m always open to discussing new projects, creative ideas or
           opportunities to be part of your visions.
         </p>
-        <button className="contact-button">Contact Me</button>
+        <button className="contact-button" onClick={() => setShowModal(true)}>
+          Contact Me
+        </button>
       </div>
+
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content expanded">
+            <button
+              className="close-button"
+              onClick={() => setShowModal(false)}
+            >
+              <FaTimes />
+            </button>
+            <h3 className="modal-title">Contact Info</h3>
+            <ul className="modal-list">
+              <li><FaPhone /> +963 964 718 061 </li>
+              <li>
+                <FaEnvelope /> thuryaamen@gmail.com
+              </li>
+              <li>
+                <FaLinkedin /> 
+                <a href="https://www.linkedin.com/in/thuraya-aldj-0a2615331"
+                  target="_blank">
+                  {" "}
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <FaGithub />
+                <a href="https://github.com/thuryaaldj" target="_blank">
+                  {" "}
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <FaFilePdf />
+                <a href="/ThurayaAldj-Front-End" target="_blank" download>
+                  {" "}
+                  Download My CV </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
